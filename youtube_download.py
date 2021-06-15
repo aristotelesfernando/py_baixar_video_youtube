df6cd9a19f4ebec2e406e5708010dc89baf8b7ca
A.Fernando Jr <aristotelesfernando.jr@gmail.com>
2021-06-15 14:25:57 -0300

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -1,7 +1,10 @@
 from pytube import YouTube
 
+##cole a URL do video desejado aqui
 video = YouTube("https://www.youtube.com/watch?v=i7ABlHngi1Q")
 
+##define a resolução do video
 video_stream = video.streams.get_highest_resolution()
 
+##pasta de salvamento/destino (não precisa por o nome do vídeo)
 video_stream.download(output_path="e:/temp")
